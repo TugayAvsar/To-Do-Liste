d464cb3e42fc7f368f75cc89cd0b0c1b57201272
TAvsar <tugay.avsar@gmx.de>
2025-04-20 19:28:02 +0200

    M2 erledigt

---
+++ frontend/src/components/ToDoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Meine To-Dos</h2>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        <strong>{{ todo.title }}</strong>: {{ todo.description }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDoList',
+  props: {
+    todos: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
